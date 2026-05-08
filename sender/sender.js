da4fb738b0439a3b6b326cbdaccd0006400a78de
vedika82 <anandvedika420@gmail.com>
2026-05-08 23:58:03 +0530

    some problems in video working

--- sender/sender.js
+++ sender/sender.js
@@ -1,4 +1,4 @@
-const webSocket = new WebSocket("")
+const webSocket = new WebSocket("ws://127.0.0.1:300")
 // in the brackets we write the url for the server
 
 // send the username to the socket server and servre stores that username 
@@ -7,16 +7,36 @@ function sendUsername(){
 
     username = document .getElementById("username-input").value
      sendData({
-        type:"store.user",
-
+        type:"store_user", 
+        // basically stores the type 
      })
 }
 
 function sendData(data){
     data.username= username
-    webSocket.senda(JSON.stringify(data))
+    webSocket.send(JSON.stringify(data))
 }
 
+
+
+let localStream
 function startCall(){
-    document.getElement
+    document.getElementById("video-call-div")
+    .style.display ="inline"
+
+    navigator.getUserMedia({
+        video:{
+            frameRate: 24,
+            width:{
+                min:480, ideal: 720, max: 1280
+            },
+            aspectRatio: 1.33333
+        }, 
+        audio: true
+    }, (stream) =>  {
+        localStream= stream
+        document.getElementById('local-video').srcObject = localStream;
+    },  (error)=>{
+        console.log(error)
+    })
 }
