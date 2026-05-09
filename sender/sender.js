bad096c24f5a9869364baba495549b8dcad07289
vedika82 <anandvedika420@gmail.com>
2026-05-09 21:44:46 +0530

    pata nhi kuch toh kiya

--- sender/sender.js
+++ sender/sender.js
@@ -1,42 +1,119 @@
-const webSocket = new WebSocket("ws://127.0.0.1:300")
+const webSocket = new WebSocket("ws://SERVER-IP-HERE:3000")
 // in the brackets we write the url for the server
+
+//to send message to the websocket
+webSocket.onmessage = (event)=>{
+    handleSignallingData(JSON.parse(event.data))
+}
+
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "answer":
+            peerConn.setRemoteDescription(data.answer)
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
+    }
+}
+
+
 
 // send the username to the socket server and servre stores that username 
 let username
-function sendUsername(){
+function sendUsername() {
 
-    username = document .getElementById("username-input").value
-     sendData({
-        type:"store_user", 
-        // basically stores the type 
-     })
+    username = document.getElementById("username-input").value
+    sendData({
+        type: "store_user"
+    })
 }
 
-function sendData(data){
-    data.username= username
+
+function sendData(data) {
+    data.username = username
     webSocket.send(JSON.stringify(data))
 }
 
 
 
 let localStream
-function startCall(){
+let peerConn
+function startCall() {
     document.getElementById("video-call-div")
-    .style.display ="inline"
+    .style.display = "inline"
 
     navigator.getUserMedia({
-        video:{
+        video: {
             frameRate: 24,
-            width:{
-                min:480, ideal: 720, max: 1280
+            width: {
+                min: 480, ideal: 720, max: 1280
             },
             aspectRatio: 1.33333
-        }, 
+        },
         audio: true
-    }, (stream) =>  {
-        localStream= stream
-        document.getElementById('local-video').srcObject = localStream;
+    }, (stream) => {
+        localStream = stream
+        document.getElementById("local-video").srcObject = localStream
+
+        let configuration ={
+            iceServers: [
+                //what is a stun server
+                {
+                    "urls": ["stun.l.google.com:19302",
+                             "stun1.l.google.com:19302",
+                             "stun2.l.google.com:19302",]
+
+                } 
+            ]
+        }
+
+        //basically the stream for peer to peer connection is used here
+        peerConn = new RTCPeerConnection(configuration)
+        peerConn.addStream(localStream)
+
+        peerConn.onaddstream = (e) => {
+            document.getElementById("remote-video")
+            .srcObject = e.stream
+        }
+
+        peerConn.onicecandidate = ((e) => {
+            if (e.candidate == null)
+                return
+            sendData({
+                type: "store_candidate",
+                candidate: e.candidate
+            })
+        })
+
+        createAndSendOffer()
+
     },  (error)=>{
         console.log(error)
     })
+}
+
+//basically can give the answer of someine to us
+function createAndSendOffer() {
+    peerConn.createOffer((offer) => {
+        sendData({
+            type: "store_offer",
+            offer: offer
+        })
+
+        peerConn.setLocalDescription(offer)
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+let isAudio = true
+function muteAudio() {
+    isAudio = !isAudio
+    localStream.getAudioTracks()[0].enabled = isAudio
+}
+
+let isVideo = true
+function muteVideo() {
+    isVideo = !isVideo
+    localStream.getVideoTracks()[0].enabled = isVideo
 }
